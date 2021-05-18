2b668829a053a31b588381fc478bb7542a8f5317
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2021-05-18 21:04:24 +0900

    first commit

---
+++ google-search/src/main/java/jp/ac/dendai/im/web/search/App.java
@@ -0,0 +1,13 @@
+package jp.ac.dendai.im.web.search;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ google-search/src/test/java/jp/ac/dendai/im/web/search/AppTest.java
@@ -0,0 +1,20 @@
+package jp.ac.dendai.im.web.search;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
